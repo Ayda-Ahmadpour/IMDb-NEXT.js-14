fabaafe1475ea3deefa7f133b59b171cad3a2523
ayda <146557313+Ayda-Ahmadpour@users.noreply.github.com>
2024-03-10 23:06:12 -0400

    complete the theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,26 @@
-// Header.jsx
 import React from "react";
 import Menu from "./MenuItems";
 import { FaHome, FaInfoCircle } from "react-icons/fa";
 import MyMovie from "../../public/MyMovie.png";
 import Image from "next/image";
 import Link from "next/link";
-import ThemeChanger from "./ThemeChanger"; // Change to default import
+import ThemeChanger from "./ThemeChanger";
 
 export default function Header() {
   return (
-    <nav className="flex justify-between w-full border items-center p-3">
-      <ul className="flex border gap-4 basis-[50%]">
+    <nav className="flex justify-between w-full items-center p-3 border-b-2 bg-red-300 border-b-orange-300">
+      <ul className="flex gap-4 h-full">
         <Menu title="Home" address="/" Icon={FaHome} />
         <Menu title="About" address="/about" Icon={FaInfoCircle} />
       </ul>
-      <div className="border flex items-center basis-[50%] gap-4 justify-end">
-        <ThemeChanger /> {/* Include ThemeChanger component */}
+      <div className="flex items-center gap-6 basis[50%]   justify-end">
+        <ThemeChanger />
         <Link href={"/"}>
-          <Image src={MyMovie} alt="" className="block w-20 cursor-pointer" />
+          <Image
+            src={MyMovie}
+            alt=""
+            className="block w-20 rounded-md cursor-pointer"
+          />
         </Link>
       </div>
     </nav>
--- src/components/ThemeChanger.jsx
+++ src/components/ThemeChanger.jsx
@@ -2,18 +2,25 @@
 import { useTheme } from "next-themes";
 import { RiSunLine } from "react-icons/ri";
 import { FaMoon } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 export default function ThemeChanger() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
   const isDark = theme === "dark" ? theme === "dark" : theme === "light";
+  useEffect(() => setMounted(true), []);
   return (
     <div>
-      <button onClick={() => setTheme("light")}>
-        <RiSunLine className="text-black text-xl hover:text-amber-400 w-5 h-full cursor-pointer" />
-      </button>
-      <button onClick={() => setTheme("dark")}>
-        <FaMoon className="text-black text-xl hover:text-amber-400 w-5 h-full cursor-pointer" />
-      </button>
+      {mounted &&
+        (theme === "dark" ? (
+          <button onClick={() => setTheme("light")}>
+            <RiSunLine className=" text-2xl hover:text-amber-300 cursor-pointer" />
+          </button>
+        ) : (
+          <button onClick={() => setTheme("dark")}>
+            <FaMoon className=" text-lg  hover:text-amber-300 cursor-pointer" />
+          </button>
+        ))}
     </div>
   );
 }
