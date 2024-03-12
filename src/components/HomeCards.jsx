6285e689ad6dd84c116d2477774bef99b00efa66
ayda <146557313+Ayda-Ahmadpour@users.noreply.github.com>
2024-03-12 10:31:01 -0400

    deigned card

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -3,7 +3,7 @@ import HomeCardsItem from "./HomeCardsItem";
 
 export default function HomeCards({ result }) {
   return (
-    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5 p-3">
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5">
       {result.map((movie) => {
         return <HomeCardsItem key={movie.id} movie={movie} />;
       })}
--- src/components/HomeCardsItem.jsx
+++ src/components/HomeCardsItem.jsx
@@ -4,21 +4,23 @@ import React from "react";
 
 export default function HomeCardsItem({ movie }) {
   return (
-    <Link href={`/movie/${movie.id}`} className="p-3">
+    <Link href={`/movie/${movie.id}`} className="p-3 mt-3">
       <div className="flex flex-col items-center gap-4 p-4 bg-gray-200 dark:bg-gray-800 rounded-md border-2 border-red-400">
         <Image
           alt="movie poster"
           src={`https://image.tmdb.org/t/p/original/${
             movie.poster_path || movie.backdrop_path
           }`}
-          width={500}
-          height={300}
+          width={400}
+          height={200}
           className="rounded-md border-2 border-gray-300 dark:border-gray-700"
         />
-        <h1 className="text-xl font-bold text-center">
-          {movie.title || movie.original_title}
+        <h1 className="text-xl font-bold text-center line-clamp-1">
+          {movie.title || movie.original_title || "Title Not Available"}
         </h1>
-        <p className="line-clamp-3 text-sm">{movie.overview}</p>
+        <p className="line-clamp-3 text-sm text-center">
+          {movie.overview || "description Not Available"}
+        </p>
       </div>
     </Link>
   );
