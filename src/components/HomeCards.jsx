4903495220ceabe68be4ad623b87a3329a430c6b
ayda <146557313+Ayda-Ahmadpour@users.noreply.github.com>
2024-03-12 10:14:57 -0400

    home cards

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -3,7 +3,7 @@ import HomeCardsItem from "./HomeCardsItem";
 
 export default function HomeCards({ result }) {
   return (
-    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5 p-3">
       {result.map((movie) => {
         return <HomeCardsItem key={movie.id} movie={movie} />;
       })}
--- src/components/HomeCardsItem.jsx
+++ src/components/HomeCardsItem.jsx
@@ -1,15 +1,25 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 export default function HomeCardsItem({ movie }) {
   return (
-    <div>
-      <Link href={`/movie/${movie.id}`}>
-        <div className="flex flex-col items-center gap-4 p-4 bg-gray-200 dark:bg-gray-800 rounded-md">
-          <h1 className="text-xl font-bold">{movie.title}</h1>
-          <p>{movie.overview}</p>
-        </div>
-      </Link>
-    </div>
+    <Link href={`/movie/${movie.id}`} className="p-3">
+      <div className="flex flex-col items-center gap-4 p-4 bg-gray-200 dark:bg-gray-800 rounded-md border-2 border-red-400">
+        <Image
+          alt="movie poster"
+          src={`https://image.tmdb.org/t/p/original/${
+            movie.poster_path || movie.backdrop_path
+          }`}
+          width={500}
+          height={300}
+          className="rounded-md border-2 border-gray-300 dark:border-gray-700"
+        />
+        <h1 className="text-xl font-bold text-center">
+          {movie.title || movie.original_title}
+        </h1>
+        <p className="line-clamp-3 text-sm">{movie.overview}</p>
+      </div>
+    </Link>
   );
 }
