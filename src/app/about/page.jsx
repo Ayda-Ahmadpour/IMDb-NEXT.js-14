0d8b7c8ad8c66825877f118546f8332d80508b70
ayda <146557313+Ayda-Ahmadpour@users.noreply.github.com>
2024-03-15 00:50:36 -0400

    about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="max-w-full mx-auto">
+    <div className="max-w-full mx-auto text-center">
       <h1 className="text-center text-amber-500 text-2xl m-10">About IMDb</h1>
-      <p className="m-auto p-3">
+      <p className="m-auto p-6 w-[80%] border border-orange-400 rounded-md">
         Welcome to my Next.js 14 IMDb Clone! Let's explore Next.js 14 features
-        and build our own IMDb-style site with Tailwind CSS. Ready to dive in
-        and build together?
+        and build our own IMDb-style site with Tailwind CSS , and ensuring
+        everything runs smoothly with Jest testing. Ready to dive in and build
+        together?
       </p>
     </div>
   );
